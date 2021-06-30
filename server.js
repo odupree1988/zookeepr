6920ef31649a7d6ccf04f4ac622c854e1bfb771e
odupree1988 <odupree1988@gmail.com>
2021-06-30 16:04:44 -0400

    fetch() the data from the server to the HTML and POST the data the user sends to the database from the HTML

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+//makes files readily available
+app.use(express.static("public"));
 
 function validateAnimal(animal) {
   if (!animal.name || typeof animal.name !== "string") {
@@ -115,6 +117,22 @@ app.post("/api/animals", (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
